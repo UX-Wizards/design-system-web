20e55ba1ea924d1faf9f09fae6775f20298628d6
Gandalf Winterwhisk <uxwizards.design@gmail.com>
2021-03-09 19:21:20 -0800

    Switch to JS

---
+++ UXWIntroduction.js
@@ -0,0 +1,21 @@
+function UXWIntroduction(props) {
+  return (
+    <div className="container">
+      <div className="flex flex-row">
+        <p className="md:w-4/5 pr-8">
+          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
+          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
+          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
+          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
+          Greedo argazdan jinn tof verpine bothan wedge.
+        </p>
+        <div className="flex-grow"></div>
+        <UXWCodePanel />
+      </div>
+      <h2 className="text-xl py-4">Installation and Usage</h2>
+      <p>
+        Hello World.
+      </p>
+    </div>
+  )
+}
