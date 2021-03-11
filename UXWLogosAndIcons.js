d4a719389874a77195395a3d8b98e8a3d96af608
sei1122 <seikoigi@gmail.com>
2021-03-10 20:42:59 -0800

    Seiko3 (#15)
    * made cards
    
    * add codne on logo and icon page
    
    * update index
    
    Co-authored-by: sei1122 <seikoigi@seikos-MBP.attlocal.net>

--- UXWLogosAndIcons.js
+++ UXWLogosAndIcons.js
@@ -1,13 +1,100 @@
 function UXWLogosAndIcons(props) {
   return (
     <div className="container">
-      <p>
-        Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-        Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-        Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-        jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-        Greedo argazdan jinn tof verpine bothan wedge.
-      </p>
+       <div className="flex-col flex-shrink-0 px-4 py-4">
+          <h1 className="text-3xl pb-4">Logos and Icons</h1>
+          <div className="container">
+            <h2 className="text-2xl pb-4">Logos</h2>
+            <p>The Wizards hat will always accompany the UX WIZARDS text.  The rectangular version will be the main logo and includes an arrow cursor as if the logo is being clicked on.</p><br/>
+            <p>The square and circle versions are to be used for things such as our Slack group’s profile.</p><br/>
+            <p className="pb-4">There are three variations in colors - white text on black, black text on white and white text on our branded Navy, Magician’s Cloak.</p>
+            <img src="assets/logos.png" alt=""/>
+            <h2 className="text-2xl pb-4 pt-16">Clearance</h2>
+            <p>To ensure that logos are clearly visible, surround with clear space that is free of type, graphics, and other elements to avoid visual clutter.</p>
+            <img src="assets/clearance.png" alt=""/>
+            <h2 className="text-2xl pb-4 pt-12">Icongraphy Style</h2>
+            <p>Icon styles explain comes here, Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss?</p>
+
+            <img className="pt-16"src="assets/icongraphy.png" alt=""/>
+
+            <h2 className="text-2xl pb-4 pt-16">Library</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col">
+                <div className="flex flex-row bg-gray-200 rounded-t-lg">
+                  <div className="text-lg">
+                    Logo Circle
+                  </div>
+                  <div className="flex-grow"></div>
+                  <div>
+                    😀
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
+                  <img className="w-32" src="logo.png" alt=""/>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-row bg-gray-200 rounded-t-lg">
+                  <div className="text-lg">
+                    Logo Circle
+                  </div>
+                  <div className="flex-grow"></div>
+                  <div>
+                    😀
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
+                  <img className="w-32" src="logo.png" alt=""/>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-row bg-gray-200 rounded-t-lg">
+                  <div className="text-lg">
+                    Logo Circle
+                  </div>
+                  <div className="flex-grow"></div>
+                  <div>
+                    😀
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
+                  <img className="w-32" src="logo.png" alt=""/>
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <div className="flex flex-row bg-gray-200 rounded-t-lg">
+                  <div className="text-lg">
+                    Logo Circle
+                  </div>
+                  <div className="flex-grow"></div>
+                  <div>
+                    😀
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
+                  <img className="w-32" src="logo.png" alt=""/>
+                </div>
+              </div>
+            </div>  
+
+            <div className="grid grid-cols-1 my-4">
+              <div className="flex flex-col">
+                <div className="flex flex-row bg-gray-200 rounded-t-lg">
+                  <div className="text-lg">
+                    Logo Circle
+                  </div>
+                  <div className="flex-grow"></div>
+                  <div>
+                    😀
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center bg-gray-100">
+                  <img className="w-4/5 py-4" src="assets/icongraphy.png" alt=""/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
