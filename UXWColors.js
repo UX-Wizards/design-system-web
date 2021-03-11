7820012fd2740da84049d9fe59ae9bd25c5400ee
Fizban UXD <78893853+FizbanUXD@users.noreply.github.com>
2021-03-10 21:17:41 -0800

    color palette png and actual text added

--- UXWColors.js
+++ UXWColors.js
@@ -2,12 +2,20 @@ function UXWColors(props) {
   return (
     <div className="container">
       <p>
-        Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-        Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-        Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-        jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-        Greedo argazdan jinn tof verpine bothan wedge.
-      </p>
+      Our palette consists of neutral, pleasing 
+tones balanced by vivid and eccentric 
+colors. This color palette has been 
+carefully chosen to provide versatility and 
+function while still providing boundless 
+room for expression. The flexible and neutral Magician’s Cloak, 
+Merlin’s Beard, and Steel lead the way, providing a well-balanced
+foundation. They are accompanied by the livelier colors of Mana,
+Acid, Charm, and Elixir. Our brighter colors should be used with
+restraintand finesse. We want our designs to stand out, but avoid
+being tacky and distasteful.
+      </ p>
+      <br/>
+      <img src="color-palette-wiz.png"/>
     </div>
   )
 }
