f2485241f9a5515c2f6f5584a2a924819fb32b69
Fizban UXD <78893853+FizbanUXD@users.noreply.github.com>
2021-03-13 00:13:04 -0800

    added text and blocks. up next: nesting grids to make text blocks

--- UXWColors.js
+++ UXWColors.js
@@ -1,13 +1,344 @@
 function UXWColors(props) {
   return (
     <div className="container">
+      <br/>
+      <br/>
+      <p> - - !  Until this tag is removed, color swatches are not accurate to UXW colors ! - - </p>
+      <br/>
+      <br/>
+
+      <br/>
+      <div className="font-bold font-2xl">
       <p>
-        Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-        Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-        Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-        jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-        Greedo argazdan jinn tof verpine bothan wedge.
+        Philosophy
       </p>
+      </div>
+      <br/>
+
+      <p>
+      Our palette consists of neutral, pleasing tones balanced by vivid
+      and eccentric colors. This color palette has been carefully chosen
+      to provide versatility and function while still providing boundless
+      room for expression.
+      </p>
+
+      <br/>
+      <br/>
+      <br/>
+
+      <div className="font-bold font-2xl">
+        <p>
+          Using our palette
+        </p>
+        </div>
+        <br/>
+
+        <p>
+        Though our palette is established, we encourage experimentation
+        through blending modes and gradients. Start with the main variant in
+        the left column, and supplement your designs with the alternate shades
+        on the right if you want to play around. You might be surprised, the
+        results could be spellbinding!
+        </p>
+
+        <br/>
+        <br/>
+        <br/>
+
+        <div className="font-bold font-2xl">
+          <p>
+            Primary colors
+          </p>
+          </div>
+          <br/>
+
+          <p>
+          The flexible and neutral Magician's Cloak, Acid, and Merlin's Beard lead the way, providing a well-balanced foundation.
+          </p>
+
+          <br/>
+          <br/>
+          <br/>
+
+          <div className="font-bold font-2xl">
+            <p>
+              Magician's Cloak
+            </p>
+          </div>
+            <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-indigo-900 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+               <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-blue-400 m-4"></div>
+                 <div className="box h-16 w-16 bg-blue-900 m-4"></div>
+               </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+
+           <div className="font-bold font-2xl">
+             <p>
+               Acid
+             </p>
+           </div>
+           <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-purple-700 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+             <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-purple-200 m-4"></div>
+                 <div className="box h-16 w-16 bg-purple-900 m-4"></div>
+             </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+           <div className="font-bold font-2xl">
+             <p>
+               Merlin's Beard
+             </p>
+           </div>
+           <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-white m-4 border-2 border-indigo-900"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+             <div className="grid grid-cols-3 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-gray-100 m-4"></div>
+                 <div className="box h-16 w-16 bg-gray-300 m-4"></div>
+                 <div className="box h-16 w-16 bg-gray-500 m-4"></div>
+             </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+           <div className="font-bold font-2xl">
+          <p>
+            Secondary colors
+          </p>
+          </div>
+          <br/>
+
+          <p>
+          The foundation colors are balanced by the livelier colors of Steel, 
+          Mana, Jinx, and Elixir. Our brighter colors should be used with 
+          restraint and finesse. We want our designs to stand out, 
+          but avoid being tacky and distasteful.
+          </p>
+
+          <br/>
+          <br/>
+          <br/>
+
+          <div className="font-bold font-2xl">
+            <p>
+              Steel
+            </p>
+          </div>
+            <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-indigo-100 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+               <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-indigo-50 m-4"></div>
+                 <div className="box h-16 w-16 bg-indigo-200 m-4"></div>
+               </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+
+           <div className="font-bold font-2xl">
+             <p>
+               Mana
+             </p>
+           </div>
+           <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-blue-500 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+             <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-blue-400 m-4"></div>
+                 <div className="box h-16 w-16 bg-blue-800 m-4"></div>
+             </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+           <div className="font-bold font-2xl">
+            <p>
+              Jinx
+            </p>
+          </div>
+            <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-pink-500 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+               <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-pink-200 m-4"></div>
+                 <div className="box h-16 w-16 bg-pink-700 m-4"></div>
+               </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+
+           <div className="font-bold font-2xl">
+             <p>
+               Elixir
+             </p>
+           </div>
+           <br/>
+
+           <div className="grid grid-cols-2 mx-4">
+             <div className="bg-indigo-300 text-center">Main variant</div>
+             <div className="bg-indigo-100 text-center">Alternate shades</div>
+           </div>
+
+
+
+           <div className="grid grid-cols-2 mx-4">
+
+             <div className="bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-yellow-500 m-4"></div>  
+             </div>
+
+             <div className="bg-indigo-100 h-32">
+             <div className="grid grid-cols-2 gap-4 m-4">
+                 <div className="box h-16 w-16 bg-yellow-200 m-4"></div>
+                 <div className="box h-16 w-16 bg-yellow-700 m-4"></div>
+             </div>
+             </div>
+
+           </div>
+
+           <br/>
+           <br/>
+           <br/>
+
+
+           <br/>
+           <br/>
+           <br/>
+
+
+           <p> - - ! Reminder, color swatches aren't to spec until this tag is removed ! - -</p>
+
+
+
+
+
+
+
     </div>
+
+
   )
 }
