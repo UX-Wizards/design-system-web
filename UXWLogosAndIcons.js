9e3edd54a02d9fe9008c5ea4b31cb77c6b2fece1
sei1122 <seikoigi@seikos-MBP.attlocal.net>
2021-03-17 15:03:29 -0700

    add sub navigation to the Logo page

--- UXWLogosAndIcons.js
+++ UXWLogosAndIcons.js
@@ -4,25 +4,25 @@ function UXWLogosAndIcons(props) {
        <div className="flex-col flex-shrink-0 px-4 py-4">
           {/* <h1 className="text-3xl pb-4">Logos and Icons</h1> */}
           <div className="container">
-            <h2 className="text-2xl pb-4">Logos</h2>
+            <h2 id="snav-logos" className="text-2xl pb-4">Logos</h2>
             <p>The Wizards hat will always accompany the UX WIZARDS text.  The rectangular version will be the main logo and includes an arrow cursor as if the logo is being clicked on.</p><br/>
             <p>The square and circle versions are to be used for things such as our Slack group’s profile.</p><br/>
             <p className="pb-4">There are three variations in colors - white text on black, black text on white and white text on our branded Navy, Magician’s Cloak.</p>
             <img src="assets/logos.png" alt=""/>
-            <h2 className="text-2xl pb-4 pt-16">Clearance</h2>
+            <h2 id="snav-clearance" className="text-2xl pb-4 pt-16">Clearance</h2>
             <p>To ensure that logos are clearly visible, surround with clear space that is free of type, graphics, and other elements to avoid visual clutter.</p>
             <img src="assets/clearance.png" alt=""/>
-            <h2 className="text-2xl pb-4 pt-12">Icongraphy Style</h2>
+            <h2 id="snav-icongraphy" className="text-2xl pb-4 pt-12">Icongraphy Style</h2>
             <p>Icon styles descriptions comes here, Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss?</p>
 
             <img className="pt-16"src="assets/icongraphy.png" alt=""/>
-            <h2 className="text-2xl pb-4 pt-16">Library</h2>
+            <h2 id="snav-library" className="text-2xl pb-4 pt-16">Library</h2>
               <p className="pb-4">Library descriptions comes here, Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss?</p>
             <div className="grid grid-cols-2 gap-4">
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
-                  <div className="text-lg py-2 pl-6">
-                    Logo Main
+                  <div className="text-base py-2 pl-6">
+                    Main Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -30,13 +30,13 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="w-32 py-4" src="logo.png" alt=""/>
+                  <img className="py-4" src="assets/logo-wide-main.svg" alt=""/>
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
-                  <div className="text-lg py-2 pl-6">
-                    Logo Main
+                  <div className="text-base py-2 pl-6">
+                    Square Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -44,13 +44,13 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="w-32 py-4" src="logo.png" alt=""/>
+                  <img className="py-4" src="assets/logo-square-main.svg" alt=""/>
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
-                  <div className="text-lg py-2 pl-6">
-                    Logo Main
+                  <div className="text-base py-2 pl-6">
+                    Circle Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -58,13 +58,13 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="w-32 py-4" src="logo.png" alt=""/>
+                  <img className="w-24 py-4" src="logo.png" alt=""/>
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
-                  <div className="text-lg py-2 pl-6">
-                    Logo Main
+                  <div className="text-base py-2 pl-6">
+                    Podcast Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -72,14 +72,14 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="w-32 py-4" src="logo.png" alt=""/>
+                  <img className="w-24 py-4" src="assets/podcast.svg" alt=""/>
                 </div>
               </div>
             </div>  
             <div className="grid grid-cols-1 my-4">
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
-                  <div className="text-lg py-2 pl-6">
+                  <div className="text-base py-2 pl-6">
                     Icongraphy
                   </div>
                   <div className="flex-grow"></div>
