685b5c90952c816ae279c6c780aefb33bdda8416
Fizban UXD <78893853+FizbanUXD@users.noreply.github.com>
2021-03-18 00:39:47 -0700

    added text to Primary section, pb-12 not impolemented for secondary yet

--- UXWColors.js
+++ UXWColors.js
@@ -1,97 +1,113 @@
 function UXWColors(props) {
   return (
     <div className="container">
-      <br/>
-      <br/>
-      <p> - - !  Until this tag is removed, color swatches are not accurate to UXW colors ! - - </p>
-      <br/>
-      <br/>
 
-      <br/>
-      <div className="font-bold font-2xl">
-      <p>
+
+      <p className="pt-8 pb-12"> - - !  Until this tag is removed, color swatches are not accurate to UXW colors ! - - </p>
+     
+
+      
+      <p className="font-bold font-2xl pb-4">
         Philosophy
       </p>
-      </div>
-      <br/>
 
-      <p>
+      <p className="pb-12">
       Our palette consists of neutral, pleasing tones balanced by vivid
       and eccentric colors. This color palette has been carefully chosen
       to provide versatility and function while still providing boundless
       room for expression.
       </p>
 
-      <br/>
-      <br/>
-      <br/>
-
-      <div className="font-bold font-2xl">
-        <p>
+        <p className="font-bold font-2xl pb-4">
           Using our palette
         </p>
-        </div>
-        <br/>
 
-        <p>
+        <p className="pb-24">
         Though our palette is established, we encourage experimentation
         through blending modes and gradients. Start with the main variant in
         the left column, and supplement your designs with the alternate shades
         on the right if you want to play around. You might be surprised, the
         results could be spellbinding!
         </p>
+       
+  
 
-        <br/>
-        <br/>
-        <br/>
-
-        <div className="font-bold font-2xl">
-          <p>
+        
+          <p className="font-bold font-2xl pb-4">
             Primary colors
           </p>
-          </div>
-          <br/>
 
-          <p>
+          <p className="pb-12">
           The flexible and neutral Magician's Cloak, Acid, and Merlin's Beard lead the way, providing a well-balanced foundation.
           </p>
 
-          <br/>
-          <br/>
-          <br/>
 
-          <div className="font-bold font-2xl">
-            <p>
+            <p className="font-bold font-2xl pb-4">
               Magician's Cloak
             </p>
-          </div>
-            <br/>
+
 
            <div className="grid grid-cols-2 mx-4">
-             <div className="bg-indigo-300 text-center">Main variant</div>
-             <div className="bg-indigo-100 text-center">Alternate shades</div>
+             <div className="bg-indigo-300 text-center pt-1">Main variant</div>
+             <div className="bg-indigo-100 text-center pt-1">Alternate shades</div>
            </div>
 
 
+  
 
-           <div className="grid grid-cols-2 mx-4">
+           <div className="grid grid-cols-2 mx-4 pb-12">  
 
              <div className="bg-indigo-300 h-32">
-                <div className="box h-32 w-32 bg-indigo-900 m-4"></div>  
+               <div className="grid grid-cols-3">
+                <div className="box h-32 w-32 bg-indigo-900 m-4 z-1">
+                  <p className="z-2 text-left text-white pl-4 pt-2"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="z-2 text-left text-white pl-4"><span className="text-white font-bold">A </span>PASS</p>
+                </div>
+                
+                <p className="text-left px-4 pt-6 col-span-2 pb-4">
+                <span className="font-bold">HEX:</span> <span className="font-normal">152145</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">21, 33, 69</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">19, 14, 0, 79</span>
+                </p>
+                </div> 
              </div>
 
+
+
              <div className="bg-indigo-100 h-32">
-               <div className="grid grid-cols-2 gap-4 m-4">
-                 <div className="box h-16 w-16 bg-blue-400 m-4"></div>
-                 <div className="box h-16 w-16 bg-blue-900 m-4"></div>
+
+               <div className="grid grid-cols-6 gap-4 m-4">
+
+
+
+                 <div className="box h-20 w-20 bg-blue-400 m-4">
+                 <p className="z-2 text-left text-white pl-2 pt-1"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="z-2 text-left text-white pl-2"><span className="text-white font-bold">A </span>PASS</p>
+                 </div>
+
+                 <p className="text-left p-4 col-span-2">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">5267A7</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">82, 103, 167</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">76, 63, 6, 0</span>
+                 </p>
+
+
+
+
+                 <div className="box h-20 w-20 bg-blue-900 m-4">
+                 <p className="z-2 text-left text-white pl-2 pt-1"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="z-2 text-left text-white pl-2"><span className="text-white font-bold">A </span>PASS</p>
+                 </div>
+                 
+                 <p className="text-left p-4 col-span-2">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">2E3E6E</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">46, 62, 110</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">93, 83, 30, 17</span>
+                 </p>
                </div>
              </div>
 
            </div>
-
-           <br/>
-           <br/>
-           <br/>
 
 
 
@@ -103,30 +119,63 @@ function UXWColors(props) {
            <br/>
 
            <div className="grid grid-cols-2 mx-4">
-             <div className="bg-indigo-300 text-center">Main variant</div>
-             <div className="bg-indigo-100 text-center">Alternate shades</div>
+             <div className="bg-indigo-300 text-center pt-1">Main variant</div>
+             <div className="bg-indigo-100 text-center pt-1">Alternate shades</div>
            </div>
 
 
 
-           <div className="grid grid-cols-2 mx-4">
+           <div className="grid grid-cols-2 mx-4 pb-12">
 
-             <div className="bg-indigo-300 h-32">
-                <div className="box h-32 w-32 bg-purple-700 m-4"></div>  
+             <div className="grid grid-cols-3 bg-indigo-300 h-32">
+                <div className="box h-32 w-32 bg-purple-700 m-4 z-1">
+                  <p className="text-left text-white pl-4 pt-2"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="text-left text-white pl-4"><span className="text-white font-bold">A </span>PASS</p>
+                  </div>
+
+                  <p className="text-left px-4 pt-6 col-span-2 pb-4">
+                <span className="font-bold">HEX:</span> <span className="font-normal">6331FD</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">99, 49, 253</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">60, 80, 0, 1</span>
+                </p>
              </div>
+
+
+
 
              <div className="bg-indigo-100 h-32">
-             <div className="grid grid-cols-2 gap-4 m-4">
-                 <div className="box h-16 w-16 bg-purple-200 m-4"></div>
-                 <div className="box h-16 w-16 bg-purple-900 m-4"></div>
+
+             <div className="grid grid-cols-6 gap-4 m-4">
+               
+                 <div className="box h-20 w-20 bg-purple-200 m-4 z-1">
+                 <p className="z-2 text-left text-black pl-2 pt-1"><span className="text-black font-bold">A </span>PASS</p>
+                  <p className="z-2 text-left text-black pl-2"><span className="text-white font-bold">A </span>FAIL</p>
+                 </div>
+
+                 <p className="text-left p-4 col-span-2">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">D5CCFB</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">213, 204, 251</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">14, 18, 0, 0</span>
+                 </p>
+
+
+                 
+                 <div className="box h-20 w-20 bg-purple-900 m-4 z-1">
+                 <p className="z-2 text-left text-white pl-2 pt-1"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="z-2 text-left text-white pl-2"><span className="font-bold">A </span>PASS</p>
+                 </div>
+
+                 <p className="text-left p-4 col-span-2">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">3822AC</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">56, 34, 172</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">90, 92, 0, 0</span>
+                 </p>
+
              </div>
              </div>
 
            </div>
 
-           <br/>
-           <br/>
-           <br/>
 
 
            <div className="font-bold font-2xl">
@@ -137,38 +186,82 @@ function UXWColors(props) {
            <br/>
 
            <div className="grid grid-cols-2 mx-4">
-             <div className="bg-indigo-300 text-center">Main variant</div>
-             <div className="bg-indigo-100 text-center">Alternate shades</div>
+             <div className="bg-indigo-300 text-center pt-1">Main variant</div>
+             <div className="bg-indigo-100 text-center pt-1">Alternate shades</div>
            </div>
 
 
 
-           <div className="grid grid-cols-2 mx-4">
+           <div className="grid grid-cols-2 mx-4 pb-28">
 
-             <div className="bg-indigo-300 h-32">
-                <div className="box h-32 w-32 bg-white m-4 border-2 border-indigo-900"></div>  
+             <div className=" grid grid-cols-3 bg-indigo-300 h-32">
+
+                <div className="box h-32 w-32 bg-white m-4 border-2 border-indigo-900 z-1">
+                <p className="z-2 text-left pl-2 pt-1"><span className="text-black font-bold">A </span>PASS</p>
+                  <p className="z-2 text-left text-black pl-2"><span className="text-white font-bold">A </span>FAIL</p>
+                  </div>
+
+                  <p className="text-left px-4 pt-6 col-span-2 pb-4">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">FFFFFF</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">255, 255, 255</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">0, 0, 0, 0</span>
+                 </p>
              </div>
 
              <div className="bg-indigo-100 h-32">
-             <div className="grid grid-cols-3 gap-4 m-4">
-                 <div className="box h-16 w-16 bg-gray-100 m-4"></div>
-                 <div className="box h-16 w-16 bg-gray-300 m-4"></div>
-                 <div className="box h-16 w-16 bg-gray-500 m-4"></div>
+
+             <div className="flex flex-row flex-wrap gap-4">
+
+                  <div className="flex flex-none">
+                 <div className="box h-20 w-20 bg-gray-100 m-4 z-1">
+                 <p className="z-2 text-left pl-2 pt-1"><span className="text-black font-bold">A </span>PASS</p>
+                  <p className="z-2 text-left text-black pl-2"><span className="text-white font-bold">A </span>FAIL</p>
+                 </div>
+                 
+
+                 <p className="text-left col-span-2 py-4">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">E6E6E6</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">230, 230, 230</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">9, 6, 7, 0</span>
+                 </p>
+                 </div>
+                 
+
+                 <div className="flex flex-none">
+                 <div className="box h-20 w-20 bg-gray-300 m-4">
+                 <p className="z-2 text-left pl-2 pt-1"><span className="text-black font-bold">A </span>PASS</p>
+                  <p className="z-2 text-left text-black pl-2"><span className="text-white font-bold">A </span>FAIL</p>
+                 </div>
+
+                 <p className="text-left col-span-2 pt-4 pb-4">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">C9CACB</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">201, 202, 203</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">21, 16, 16, 0</span>
+                 </p>
+                 </div>
+
+
+                 <div className="flex flex-none flex-grow bg-indigo-100 pb-2">
+                 <div className="box h-20 w-20 bg-gray-500 m-4">
+                 <p className="z-2 text-left text-white pl-2 pt-1"><span className="text-black font-bold">A </span>FAIL</p>
+                  <p className="z-2 text-left text-white pl-2"><span className="text-white font-bold">A </span>PASS</p>
+                 </div>
+
+                 <p className="text-left col-span-2 pt-4 pb-4">
+                 <span className="font-bold">HEX:</span> <span className="font-normal">747887</span><br/>
+                <span className="font-bold">RGB:</span> <span className="font-normal">116, 120, 135</span><br/>
+                <span className="font-bold">CMYK:</span> <span className="font-normal">58, 48, 36, 7</span>
+                 </p>
+                 </div>
+
+
              </div>
              </div>
 
            </div>
 
-           <br/>
-           <br/>
-           <br/>
-
-           <br/>
-           <br/>
-           <br/>
-
-
-           <div className="font-bold font-2xl">
+           
+           <div className="font-bold font-2xl pt-20">
           <p>
             Secondary colors
           </p>
