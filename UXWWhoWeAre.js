172bfddcb7d1a2a9de529336a948aba4cf1e2f3a
Laura Williams <lewilliams@ISABELLE.local>
2021-03-22 21:55:02 -0700

    added content

--- UXWWhoWeAre.js
+++ UXWWhoWeAre.js
@@ -2,79 +2,52 @@ function UXWWhoWeAre(props) {
   return (
     <div className="container">
       <UXWSubsection anchor="about--about" title="About Us">
-        <h2>About Us</h2>
         <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+          UX Wizards is a community of passionate, supportive learners and UX enthusiasts. 
         </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+        <p>
+          Together we host a variety of events each month that include casual hangouts, game nights, and learning events. You can find us active on the UX Wizards MeetUp group, our dedicated Slack, and our hangout Discord.
         </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+        <p>
+          Membership is completely free! We welcome all that want to participate and join with the intention of making real connections, a willingness to support others, and an interest in learning together.
         </p>
       </UXWSubsection>
       <UXWSubsection anchor="about--mission" title="Our Mission">
-        <h2>Our Mission</h2>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+        <p className="mt-8 italic">
+         Our mission is a compass to guide our decisions made in the name of the community. It ensures that every event we hold, every project we start, and every piece of content we create is working toward the same goal in order to best serve our fellow Wizards.
         </p>
       </UXWSubsection>
       <UXWSubsection anchor="about--personality" title="Personality">
-        <h2>Personality</h2>
         <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+          Fun &amp; Whimsical<br />
+          And occasionally nerdy. 🤓 We’re unpretentious, playful, and love a good joke or pun. 
         </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
+        <p>
+          Passionate &amp; Excited <br />
+          Eager to learn and share knowledge with others
+        </p>
+        <p> 
+         Motivated &amp; Scrappy <br />
+          We’re willing to take risks and just get out there. Doesn’t need a title to be a leader. We encourage everyone to give it a whirl.
+        </p>
+        <p>
+        Empathetic &amp; Approachable <br />
+        Understanding, unpretentious, and accessible.
+        </p>
+        <p>
+        Adventurous &amp; Imaginative <br />
+        Interested in ideas that are a little out there even if it’s just a random thought
         </p>
       </UXWSubsection>
       <UXWSubsection anchor="about--values" title="Values">
-       <h2>Values</h2>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
+        <ul class="list-disc list-inside">
+          <li>Friendship - Doing things with friends makes it all that much more fun</li>
+          <li>Curiosity - A passion to learn. There is no such thing as a stupid question</li>
+          <li>Inclusion - All are welcome. People, opinions, backgrounds. All.</li>
+          <li>Growth - We’re not looking for experts or perfection. We strive for new experiences, never perfection. </li>
+          <li>Collaboration - Share knowledge and work together. Learn from others and in return teach others, so we may rise together</li>
+          <li>Initiative - We wouldn’t be here without the volunteers that step up and get things done.</li>
+        </ul>
       </UXWSubsection>
     </div>
   )
