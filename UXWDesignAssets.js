bad5b10db551f945f0cd0d92bf93931ed8d73c9d
Javier Garcia <javier.garcia549@gmail.com>
2021-03-29 21:31:41 -0700

    Add iframe

--- UXWDesignAssets.js
+++ UXWDesignAssets.js
@@ -2,6 +2,7 @@ function UXWDesignAssets(props) {
   return (
     <div className="container">
       <p className="pb-6">Download the latest version of our brand guide for Adobe XD</p>
+      <iframe src="https://xd.adobe.com/view/e296d376-c859-48e3-980f-07065327cdcf-b7c5/?hints=off" className="h-screen w-full pb-16" />
       <a className="text-white py-4 px-12 from-indigo-600 to-indigo-900 bg-gradient-to-r rounded-md uppercase tracking-widest font-light hover:shadow-2xl transition duration-500 ease-in-out hover:bg-opacity-40" href="assets/BrandGuide_Main_1.2.xd" download>Download</a>
     </div>
   )
