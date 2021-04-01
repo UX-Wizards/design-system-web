be6450a39859df03508dd682df77630c8612d552
Javier Garcia <javier.garcia549@gmail.com>
2021-03-31 20:21:05 -0700

    Remove style tag

--- UXWDesignAssets.js
+++ UXWDesignAssets.js
@@ -3,7 +3,7 @@ function UXWDesignAssets(props) {
     <div className="container">
       <p className="pb-6">Download the latest version of our brand guide for Adobe XD</p>
       <a className="text-white py-4 px-12 from-indigo-600 to-indigo-900 bg-gradient-to-r rounded-md uppercase tracking-widest font-light hover:shadow-2xl transition duration-500 ease-in-out hover:bg-opacity-40" href="assets/BrandGuide_Main_1.2.xd" download>Download</a>
-      <iframe src="https://xd.adobe.com/embed/e296d376-c859-48e3-980f-07065327cdcf-b7c5/" className="w-full pb-16" style="" />
+      <iframe src="https://xd.adobe.com/embed/e296d376-c859-48e3-980f-07065327cdcf-b7c5/" className="w-full pb-16" />
     </div>
   )
 }
