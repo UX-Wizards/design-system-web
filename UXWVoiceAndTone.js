07d7e82703ca111174974ed7827f6ff6fe00a413
Laura W <50504435+lewilliams89@users.noreply.github.com>
2021-04-04 16:33:18 -0700

    added content (#29)
    Co-authored-by: Laura Williams <lewilliams@ISABELLE.local>

--- UXWVoiceAndTone.js
+++ UXWVoiceAndTone.js
@@ -1,47 +1,37 @@
 function UXWHowWeSound(props) {
   return (
     <div className="container">
-      <UXWSubsection anchor="voice--voice" title="Voice">
-        <h2>Voice</h2>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-      </UXWSubsection>
-      <UXWSubsection anchor="voice--tone" title="Tone">
-        <h2>Tone</h2>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-        <p className="mt-8">
-          Lucas ipsum dolor sit amet carnor needa chazrach rebo sebulba plagueis finis di veknoid ventress.
-          Maul lando vader sy hutt nomi. Gordin haruun bail jar. Fett mothma toydarian jerec greedo bothawui sesswenna artaru zuckuss.
-          Nassau cerea sern naboo. Tagge leia obi-wan thennqora antilles. Davin selkath jobal tavion metalorn organa muunilinst jettster.
-          jinn hutt ikrit vor. Bertroff kael hutt wharl oola dagobah bibble saleucami. Colton raioballo ansionian hapan billaba wampa organa.
-          Greedo argazdan jinn tof verpine bothan wedge.
-        </p>
-      </UXWSubsection>
+        <UXWSubsection anchor="voice--voice" title="Voice">
+          <p className="pb-4">
+            Wizards focus on people first. We never want to overtalk people or use unnecessarily complex language. We’re smart and we know it; we don’t have to prove it with language.
+            </p>
+          <p className="pb-4">
+            Wizards are quirky but never silly. We enjoy a good laugh and strokes of comedy, but we also are mindful of our ethos and the validity of our expertise.
+            </p>
+          <p className="pb-4">
+            Wizards get frustrated just like everyone else, but we do our best to keep it clean. We create content that distances itself from cursing but, if done with intent, we will consider it.
+            </p>
+        </UXWSubsection>
+        <UXWSubsection anchor="voice--tone" title="Tone of Voice">
+          <div className="flex p-4 mb-4 w-1/2 justify-center uxw-bg-magicians-cloak-1 rounded-lg">
+            <p className="text-white">
+              Whimsical ◆ Witty ◆ Encouraging ◆ Nerdy
+            </p>
+          </div>
+          <p className="pb-4">
+            UX Wizards’ tone of voice is unique to our group and true to our identity at all times.
+             </p>
+          <p className="pb-4">
+            We’re knowledgeable. Our writing inspires confidence in our ability to be a reliable resource for information.
+            </p>
+          <p className="pb-4">
+            We’re witty. We can appreciate good humor, and we never take ourselves too seriously.
+            We’re encouraging. We have a can-do attitude and inspire our members to push their abilities to the next level.
+            </p>
+          <p className="pb-4">
+            We’re nerdy, and we’re unapologetic about it.
+            </p>
+        </UXWSubsection>
     </div>
   )
 }
