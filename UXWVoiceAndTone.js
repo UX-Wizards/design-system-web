c8a81d17c00833fc8f21e4b1e26dd14080d16373
Laura W <50504435+lewilliams89@users.noreply.github.com>
2021-04-05 21:57:56 -0700

    small edit to box styling (#38)
    Co-authored-by: Laura Williams <lewilliams@ISABELLE.local>

--- UXWVoiceAndTone.js
+++ UXWVoiceAndTone.js
@@ -4,33 +4,33 @@ function UXWHowWeSound(props) {
         <UXWSubsection anchor="voice--voice" title="Voice">
           <p className="pb-4">
             Wizards focus on people first. We never want to overtalk people or use unnecessarily complex language. We’re smart and we know it; we don’t have to prove it with language.
-            </p>
+          </p>
           <p className="pb-4">
             Wizards are quirky but never silly. We enjoy a good laugh and strokes of comedy, but we also are mindful of our ethos and the validity of our expertise.
-            </p>
+          </p>
           <p className="pb-4">
             Wizards get frustrated just like everyone else, but we do our best to keep it clean. We create content that distances itself from cursing but, if done with intent, we will consider it.
-            </p>
+          </p>
         </UXWSubsection>
         <UXWSubsection anchor="voice--tone" title="Tone of Voice">
-          <div className="flex p-4 mb-4 w-1/2 justify-center uxw-bg-magicians-cloak-1 rounded-lg">
-            <p className="text-white">
+          <div className="flex w-1/2 py-4 px-2 mt-4 mb-4 justify-center uxw-bg-steel-2 rounded-lg border-black rounded-md shadow-inner items-center">
+            <p>
               Whimsical ◆ Witty ◆ Encouraging ◆ Nerdy
             </p>
           </div>
           <p className="pb-4">
             UX Wizards’ tone of voice is unique to our group and true to our identity at all times.
-             </p>
+          </p>
           <p className="pb-4">
             We’re knowledgeable. Our writing inspires confidence in our ability to be a reliable resource for information.
-            </p>
+          </p>
           <p className="pb-4">
             We’re witty. We can appreciate good humor, and we never take ourselves too seriously.
             We’re encouraging. We have a can-do attitude and inspire our members to push their abilities to the next level.
-            </p>
+          </p>
           <p className="pb-4">
             We’re nerdy, and we’re unapologetic about it.
-            </p>
+          </p>
         </UXWSubsection>
     </div>
   )
