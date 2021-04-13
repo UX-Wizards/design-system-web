34528ea73fe3cfeb593ebf22203c61f65c44c918
Laura Williams <lewilliams@ISABELLE.local>
2021-04-12 23:36:55 -0700

    Content WIP

--- UXWHowWeSound.js
+++ UXWHowWeSound.js
@@ -1,37 +1,68 @@
 function UXWHowWeSound(props) {
   return (
     <div className="container">
-        <UXWSubsection anchor="voice--voice" title="Voice">
-          <p className="pb-4 max-w-prose">
-            Wizards focus on people first. We never want to overtalk people or use unnecessarily complex language. We’re smart and we know it; we don’t have to prove it with language.
-          </p>
-          <p className="pb-4 max-w-prose">
-            Wizards are quirky but never silly. We enjoy a good laugh and strokes of comedy, but we also are mindful of our ethos and the validity of our expertise.
-          </p>
-          <p className="pb-4 max-w-prose">
-            Wizards get frustrated just like everyone else, but we do our best to keep it clean. We create content that distances itself from cursing but, if done with intent, we will consider it.
-          </p>
-        </UXWSubsection>
-        <UXWSubsection anchor="voice--tone" title="Tone of Voice">
-          <div className="flex max-w-prose py-4 px-2 mt-4 mb-4 justify-center uxw-bg-steel-2 rounded-lg border-black rounded-md shadow-inner items-center">
-            <p>
-              Whimsical ◆ Witty ◆ Encouraging ◆ Nerdy
-            </p>
-          </div>
-          <p className="pb-4 max-w-prose">
-            UX Wizards’ tone of voice is unique to our group and true to our identity at all times.
-          </p>
-          <p className="pb-4 max-w-prose">
-            We’re knowledgeable. Our writing inspires confidence in our ability to be a reliable resource for information.
-          </p>
-          <p className="pb-4 max-w-prose">
-            We’re witty. We can appreciate good humor, and we never take ourselves too seriously.
-            We’re encouraging. We have a can-do attitude and inspire our members to push their abilities to the next level.
-          </p>
-          <p className="pb-4 max-w-prose">
-            We’re nerdy, and we’re unapologetic about it.
-          </p>
-        </UXWSubsection>
+      <UXWSubsection anchor="voice--referencing" title="Referencing">
+        <p className="pb-4 max-w-prose">
+          When referring to the group, we use “UX Wizards” for external, public-facing copy.
+        </p>
+        <p className="pb-4 max-w-prose">
+          This may be abbreviated “UXW” in more informal or internal documentation, but should be introduced in parentheses after the full terminology for its first use <span className="italic text-gray-500"> — e.g. &quot; UX Wizards (UXW) is the bee’s knees!&quot;</span>
+        </p>
+        <p className="pb-4 max-w-prose">
+          We may informally refer to ourselves or other members as Wizards.
+        </p>
+      </UXWSubsection>
+      <UXWSubsection anchor="voice--voice" title="Voice">
+        <p className="pb-4 italic max-w-prose">
+          Our voice is what remains consistent across all the content we create. It should reflect our values and personality and define us easily.
+        </p>
+        <p className="pb-4 max-w-prose">
+          We focus on people first - in a way where we consider everyone a friend. Our voice is inclusive and inviting, always eager to get our friend to join us in the fun but never to the point of being overbearing.
+        </p>
+        <p className="pb-4 max-w-prose">
+          We respect our friends and above all we want them to do what makes them happy. If they’re not ready to join us, there is no pressure because like a good friend we are always here for them when they’re ready. We are understanding, kind, and patient.
+        </p>
+        <p className="pb-8 max-w-prose">
+          Still, we can be a little teasing. What’s a little fun between friends after all? Jokes and puns let them know we don’t take anything too seriously.
+        </p>
+
+      </UXWSubsection>
+      <UXWSubsection anchor="voice--tone" title="Tone of Voice">
+        <p className="pb-4 italic max-w-prose">
+          UX Wizards’ tone of voice is unique to our group and true to our identity at all time, but changes depending on our message. This should guide you in conveying different attitudes.
+        </p>
+        <div className="flex max-w-prose py-4 px-2 mt-4 mb-4 justify-center uxw-bg-steel-2 rounded-lg border-black rounded-md shadow-inner items-center">
+          <p>
+            Expressive ◆ Happy ◆ Encouraging ◆ Nerdy
+        </p>
+        </div>
+        <p className="pb-4 max-w-prose">
+          We’re knowledgeable. Our writing inspires confidence in our ability to be a reliable resource for information.
+        </p>
+        <p className="pb-4 max-w-prose">
+          We’re witty. We can appreciate good humor, and we never take ourselves too seriously.
+          We’re encouraging. We have a can-do attitude and inspire our members to push their abilities to the next level.
+        </p>
+        <p className="pb-4 max-w-prose">
+          We’re nerdy, and we’re unapologetic about it.
+        </p>
+      </UXWSubsection>
+
+      <UXWSubsection anchor="voice--guide" title="Styling Guide">
+        <p className="pb-4 italic max-w-prose">
+          Styling tips
+        </p>
+        <p className="pb-4 max-w-prose">
+          this
+        </p>
+        <p className="pb-4 max-w-prose">
+          is
+        </p>
+        <p className="pb-4 max-w-prose">
+          text
+        </p>
+      </UXWSubsection>
+
     </div>
   )
 }
