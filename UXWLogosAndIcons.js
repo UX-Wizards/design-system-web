6f777f00c680d3ebb7d18571a40dc24bf115e2cf
seiko igi <seikoigi@gmail.com>
2021-10-26 12:49:10 -0700

    updated content

--- UXWLogosAndIcons.js
+++ UXWLogosAndIcons.js
@@ -4,26 +4,38 @@ function UXWLogosAndIcons(props) {
       <div className="flex-col flex-shrink-0 py-4">
         <div className="container">
           <UXWSubsection anchor="snav-logos" title="Logos">
-            <p className="max-w-prose">The Wizards hat will always accompany the UX Wizards text.  The rectangular version will be the main logo and includes an arrow cursor as if the logo is being clicked on.</p><br />
-            <p className="max-w-prose">The square and circle versions are to be used for things such as our Slack group’s profile.</p><br />
-            <p className="pb-4 max-w-prose">There are three variations in colors - white text on black, black text on white and white text on our branded Navy, Magician’s Cloak.</p>
-            <img src="assets/logos.png" alt="" />
+            <p className="max-w-prose">Our primary logo combines our brand mark, wizard hat, and the UX WIZARDS text. Wherever possible, we aim to use the primary logo in full-color.</p><br />
+            <p className="max-w-prose">The circle logo variations are to be used as social media icons and graphics. Word Mark is often incoorporate with graphic and for the banners. </p><br />
           </UXWSubsection>
-          <UXWSubsection anchor="snav-clearance" title="Clearance">
-            <p className="max-w-prose">To ensure that logos are clearly visible, surround with clear space that is free of type, graphics, and other elements to avoid visual clutter.</p>
-            <img src="assets/clearance.png" alt="" className="w-3/4"/>
+          <UXWSubsection anchor="snav-clearance" title="Primary Logo">
+            <p className="max-w-prose">The main logo should be used in this form whenever possible. The inverted version features a greater legibility on dark or busy backgrounds.</p>
+            <img src="assets/primary_logo.png" alt="" className="w-2/4"/>
           </UXWSubsection>
-          <UXWSubsection anchor="snav-icongraphy" title="Icongraphy Style">
-            <p className="max-w-prose">Icon designs are intended to be recognized and communicate immediately. Our main icons are outlined style which use a clear stroke and the Magician’s Cloak navy color. Icon + light purple circle background is a variation. This background pops and draws extra attention to the icons. </p><br />
-            <img className="my-16 w-3/4" src="assets/icongraphy.png" alt="" />
+          <UXWSubsection anchor="snav-icongraphy" title="Monotone Logos">
+            <p className="max-w-prose">When our primary or full-color logos aren’t an option, use the monotone logo that provides the most contrast. </p><br />
+            <img className="w-2/4" src="assets/monotone_logo.png" alt="" className="w-2/4" />
           </UXWSubsection>
+          <UXWSubsection anchor="snav-icongraphy" title="Word Mark">
+            <img className="w-3/4" src="assets/word_mark.png" alt="" />
+          </UXWSubsection>
+          <UXWSubsection anchor="snav-icongraphy" title="Clearance">
+            <img className="w-3/4" src="assets/clearance.png" alt="" />
+          </UXWSubsection>
+          <UXWSubsection anchor="snav-icongraphy" title="Icon Style">
+          <p className="max-w-prose">Icon designs are intended to be recognized and communicate immediately. </p><br />
+          <p className="max-w-prose">Our main icons are outlined style which use a clear stroke and the Magician’s Cloak navy color. </p><br />
+          <p className="max-w-prose">Icon + light purple circle background is a variation. This background pops and draws extra attention to the icons. </p><br />
+          {/* <p className="max-w-prose">Icon designs are intended to be recognized and communicate immediately. Our main icons are outlined style which use a clear stroke and the Magician’s Cloak navy color. Icon + light purple circle background is a variation. This background pops and draws extra attention to the icons. </p><br /> */}
+            <img className="w-3/4" src="assets/icon_style.png" alt="" />
+          </UXWSubsection>
+          
           <UXWSubsection anchor="snav-library" title="Downloads">
             <p className="pb-16 max-w-prose">Each download includes .png and .svg formats. Also, it comes with other color options.</p>
             <div className="grid grid-cols-2 gap-8">
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
                   <div className="text-base py-2 pl-6">
-                    Main Logo
+                  Primary Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -33,13 +45,13 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="py-8" src="assets/logo-wide-main.svg" alt="" />
+                  <img className="py-8" src="assets/logo-primary.svg" alt="" />
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
                   <div className="text-base py-2 pl-6">
-                    Square Logo
+                  Circle Logo
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -49,13 +61,13 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="py-8" src="assets/logo-square-main.svg" alt="" />
+                  <img className="py-8" src="assets/logo-circle.svg" alt="" />
                 </div>
               </div>
               <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
                   <div className="text-base py-2 pl-6">
-                    Circle Logo
+                  Word Mark
                   </div>
                   <div className="flex-grow "></div>
                   <div className="py-2 pr-4">
@@ -65,10 +77,10 @@ function UXWLogosAndIcons(props) {
                   </div>
                 </div>
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
-                  <img className="py-8" src="assets/logo-circle-main.svg" alt="" />
+                  <img className="py-16" src="assets/logo-wordmark.svg" alt="" />
                 </div>
               </div>
-              <div className="flex flex-col">
+              {/* <div className="flex flex-col">
                 <div className="flex flex-row bg-gray-200 rounded-t-lg">
                   <div className="text-base py-2 pl-6">
                     Podcast Logo
@@ -83,7 +95,7 @@ function UXWLogosAndIcons(props) {
                 <div className="flex flex-row justify-center bg-gray-100 rounded-b-lg">
                   <img className="py-8" src="assets/podcast.png" alt="" />
                 </div>
-              </div>
+              </div> */}
             </div>
             <div className="grid grid-cols-1 my-8">
               <div className="flex flex-col">
