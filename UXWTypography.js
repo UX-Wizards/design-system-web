230da7fabecc08f83dbfbc21742c00658f22d253
Dung Nguyen <dung.t.nguy3n@gmail.com>
2021-11-18 09:35:32 -0800

    Removed old test for tables
    Was testing a code for table to make it easier to display.

--- UXWTypography.js
+++ UXWTypography.js
@@ -145,13 +145,7 @@ function UXWTypography(props) {
           
 
 
-          <t-table
-  :headers="['Name', 'Email', 'Age', 'Sales']"
-  :data="[
-    ['Alfonso Bribiesca', 'alfonso@vexilo.com', '31', '$9,999.00'],
-    ['Saida Redondo', 'saida@gmail.com', 27, '$124.00'],
-  ]"
-></t-table>
+          
           
 
       </UXWSubsection>
