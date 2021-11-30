a12e793405f34863f92cc925ff4c173ed25f790b
seiko igi <seikoigi@gmail.com>
2021-11-29 16:09:22 -0800

    add 3 components on component page

--- UXWComponent.js
+++ UXWComponent.js
@@ -16,6 +16,33 @@ function UXWComponent(props) {
         Read more about <a href="/introduction" target="_blank">Installation and Usage </a>to learn how this works. 
         </p>
         <div class="flex max-w-prose p-4 my-4 uxw-bg-steel-2 rounded-lg border-black rounded-md shadow-inner"><p class="max-w-prose">Use CSS class names on your HTML elements to apply the style. You don’t need to write CSS once you included <span className="uxw-code">uxwcore.css </span> and <span className="uxw-code">uxw.css</span> on your file.</p></div>
+        <div class="max-w-7xl grid grid-cols-3 gap-8 my-10">
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Color Classes</p>
+            <img src="assets/comp-1.svg" class="mx-auto my-10" alt=""/>
+          </div>
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Buttons</p>
+            <img src="assets/comp-2.svg" class="mx-auto my-10" alt=""/>
+          </div>
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Cards</p>
+            <img src="assets/comp-3.svg" class="mx-auto my-10" alt=""/>
+          </div>
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Color Classes</p>
+            <img src="assets/comp-1.svg" class="mx-auto my-10" alt=""/>
+          </div>
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Buttons</p>
+            <img src="assets/comp-2.svg" class="mx-auto my-10" alt=""/>
+          </div>
+          <div class="py-4 px-6 bg-gray-100 text-lg font-bold">
+            <p>Cards</p>
+            <img src="assets/comp-3.svg" class="mx-auto my-10" alt=""/>
+          </div>
+         
+        </div>
       </UXWSubsection>
     </div>
   )
